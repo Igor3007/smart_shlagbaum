5829e6584571144c28150f749ecad5f9db257559
DenOn1x <den.onix2002@gmail.com>
2023-06-30 14:40:44 +0300

    mod2.0

---
+++ src/blocks/modules/franchise-text-block/franchise-text-block.js
@@ -0,0 +1,13 @@
+modules.define('franchise-text-block', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
